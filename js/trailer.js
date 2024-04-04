6dde379dcf1b9380d15bf408d0c4e6fc294417ae
Yerko Barriga <yerkobarriga14@gmail.com>
2024-04-04 18:29:17 -0400

    icono-responsive

--- js/trailer.js
+++ js/trailer.js
@@ -179,20 +179,6 @@ function verCategoria(cat){
 
 verCategoria("item");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Función asíncrona para buscar películas en el JSON
 async function buscarPeliculas(termino) {
     
@@ -257,3 +243,40 @@ inputBusqueda.addEventListener('input', async () => {
         console.error('Error al buscar películas:', error);
     }
 });
+
+function responsiveMenu(){
+    let x = document.getElementById("nav");
+    if(x.className===""){
+        x.className = "responsive";
+        
+        //creamos el elemento que cierra el menu
+        let span = document.createElement("span");
+        span.innerHTML = "X";
+        document.getElementById("nav").appendChild(span);
+
+        //quitamos el boton eliminar cuando se hace click sobre este
+        span.onclick = function(){
+            x.className = "";
+            span.remove();
+        }
+    }else{
+        x.className="";
+    }
+}
+
+//Este codigo es para agregar una función a cada links del menu
+//responsive
+//cuando se presione en cualquier de los links del menu debe desaparecer el menu
+const links = document.querySelectorAll("#nav a");
+console.log(links);
+for(i=0; i < links.length;i++){
+    links[i].onclick = function(){
+        var x = document.getElementById("nav");
+        x.className = "";
+
+        //removemos el boton eliminar
+        btnEliminar = document.querySelector("#nav span");
+        btnEliminar.remove();
+        
+    }
+}
